2c2b4d567f58ec9bd35337b870a3e5a28152ed5e
Ricardo De La Cruz <delacruzmartinezricardoe@gmail.com>
2021-10-17 18:50:59 -0400

    Add README.md and cicloDeUnTriangulo.js and fizzBuzz.js and tableroDeAjedrez.js

---
+++ cicloDeUnTriangulo.js
@@ -0,0 +1,13 @@
+/*Escriba un ciclo que haga siete llamadas a console.log para generar el siguiente triángulo:
+
+#
+##
+###
+####
+#####
+######
+#######*/
+
+for (let numero = '#'; numero.length <= 7; numero+='#'){
+    console.log(numero);
+}
