b05d957c9e8466b63ca94b7bd74e07d6d2d0d0c7
Ricardo De La Cruz <delacruzmartinezricardoe@gmail.com>
2021-10-18 12:57:08 -0400

    Edit Cheesboard.js

--- Chessboard.js
+++ Chessboard.js
@@ -10,3 +10,21 @@ Passing this string to console.log should show something like this:
 # # # # 
  # # # #
 # # # #*/
+let s = "";
+let l = " ";
+//This cycle is responsible for doing the numeral symbol on the same line 4 times
+for(let i = 1; i <= 4; i++) {
+  let numeral = String(i);
+  numeral = "#";
+  s += numeral + " ";
+}
+//this cycle is responsible of doing the 8 lines.
+  l = parseInt(s);
+  for(l = 1; l <= 8; l++){
+    if(l % 2 == 0){
+      console.log(s);
+    }
+    else{
+      console.log(" " + s);
+    }
+}
